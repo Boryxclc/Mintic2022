6e8b3a21c674fd64aafbf749f618b0d4fc8b0650
Borys De La Cruz <boryxdlc@gmail.com>
2022-09-19 13:20:53 -0500

    segundo commit

--- sistemaPaquetes/sistemaPaquetes/settings.py
+++ sistemaPaquetes/sistemaPaquetes/settings.py
@@ -32,12 +32,13 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'gestionPaquetes',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
